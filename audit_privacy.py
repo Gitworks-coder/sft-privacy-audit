21e8e1345c79aa5ee9d2aa6425bfb89d40baeca1
Gitworks-coder <thierrydiderotkam@gmail.com>
2026-05-21 00:54:27 +0200

    Update audit_privacy.py

--- audit_privacy.py
+++ audit_privacy.py
@@ -1,1 +1,6 @@
-
+import torch
+import pandas as pd
+import numpy as np
+from transformers import AutoTokenizer, AutoModelForCausaLLM
+from presidio_analizer import AnalyserEngine
+from sklearn.metrics.pairwise import cosine_similarity
