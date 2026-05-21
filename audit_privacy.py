449d01bd4498fe4e1adda4424029c836b8e42442
Gitworks-coder <thierrydiderotkam@gmail.com>
2026-05-21 19:57:08 +0200

    Update audit_privacy.py

--- audit_privacy.py
+++ audit_privacy.py
@@ -1,6 +1,14 @@
 import torch
 import pandas as pd
 import numpy as np
-from transformers import AutoTokenizer, AutoModelForCausaLLM
+from transformers import AutoTokenizer, AutoModelForCausalLM
 from presidio_analizer import AnalyserEngine
 from sklearn.metrics.pairwise import cosine_similarity
+#CONNECTING TO THE MODEL OF TEST DATA
+print("Uploading the small size model data (Pythia-70m)")
+model_name = "EleutherAI/pythia-70m"
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=True)
+model.eval()
+# SIMULATION OF A REAL-WORLD SFT dataset (Input/Output)
+# One of the examples intentionnally containing a Personnally Identifiable Information (PII)
