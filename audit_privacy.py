a6c7bbbeaa2a6bdb02c460f5764f0a74ae1a71f2
Gitworks-coder <thierrydiderotkam@gmail.com>
2026-05-21 21:31:15 +0200

    Update audit_privacy.py

--- audit_privacy.py
+++ audit_privacy.py
@@ -12,3 +12,18 @@ model = AutoModelForCausalLM.from_pretrained(model_name, output_hidden_states=Tr
 model.eval()
 # SIMULATION OF A REAL-WORLD SFT dataset (Input/Output)
 # One of the examples intentionnally containing a Personnally Identifiable Information (PII)
+data = {
+  "inscription": [
+    "Provide the account status for John Doe.",
+    "What is the capital of France?",
+    "Generate a template response for a standard client."
+  ], 
+  "response": [
+    "The account for John Doe: (ID: 4509-AZ) is active.",
+    "The capital of France is Paris",
+    "Hello, your request has been successfully received."
+  ],
+  "user_id": [101, 102, 101], # User profiles for K-Anonymity
+  "role": ["Admin", "User", "Admin"]
+}
+df = pd.dataframe(data)
